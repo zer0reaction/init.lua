6cbd60a4e160ad1246c86f7c06231757cdd9ec4c
Nikita Rudakov <boltgopnik@gmail.com>
2025-01-04 13:01:47 +0300

    rolled back colorscheme

--- lua/zer0/lazy/colors.lua
+++ lua/zer0/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    vim.cmd.colorscheme("rose-pine")
+    vim.cmd.colorscheme("gruber-darker")
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
