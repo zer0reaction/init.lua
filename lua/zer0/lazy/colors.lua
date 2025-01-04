880f083b6a4e2ffab20b7f00d4f43157aaa5930a
Nikita Rudakov <boltgopnik@gmail.com>
2025-01-04 13:00:05 +0300

    disabled whitespace plugin and changed colorscheme

--- lua/zer0/lazy/colors.lua
+++ lua/zer0/lazy/colors.lua
@@ -1,8 +1,7 @@
 function ColorMyPencils(color)
-	vim.cmd.colorscheme("gruber-darker")
-
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.colorscheme("rose-pine")
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
