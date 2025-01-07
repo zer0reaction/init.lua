cba442ab2b54a77ae13e04db5bafc81a2c8d2ecc
Nikita Rudakov <boltgopnik@gmail.com>
2025-01-07 21:26:16 +0300

    removed listchars. meh

--- lua/zer0/set.lua
+++ lua/zer0/set.lua
@@ -43,7 +43,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.cursorline=true
 vim.opt.mouse=""
 vim.opt.list=true
-vim.opt.listchars="space:·"
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
