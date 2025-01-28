44c996e66f99e640310b352b784bbc456bc5a55b
zer0reaction <boltgopnik@gmail.com>
2025-01-28 17:24:34 +0300

    changed colorscheme for test

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    vim.cmd.colorscheme("rose-pine-moon")
+    vim.cmd.colorscheme("tokyonight-moon")
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
