2655433449f0478d8115effe7b6c4da16ac82421
zer0reaction <boltgopnik@gmail.com>
2025-02-23 22:39:51 +0300

    Changed ctrl to alt

--- lua/zer0/config/remap.lua
+++ lua/zer0/config/remap.lua
@@ -26,7 +26,7 @@ vim.keymap.set("n", "<C-j>", ":tabprev<Enter>")
 vim.keymap.set("n", "<C-k>", ":tabnext<Enter>")
 
 -- Fix visual block weirdness (and saves my pinky)
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<M-c>", "<Esc>")
 
 -- Remove highlight
 
