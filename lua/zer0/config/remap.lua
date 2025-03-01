fc3e1303f7fa092bfb8b0ed9615ad1a9b9c970c1
zer0reaction <boltgopnik@gmail.com>
2025-03-01 10:35:30 +0300

    Remove background process keybind

--- lua/zer0/config/remap.lua
+++ lua/zer0/config/remap.lua
@@ -26,3 +26,6 @@ vim.keymap.set("n", "<C-k>", ":tabnext<Enter>")
 
 -- Fix visual block weirdness (and saves my pinky)
 vim.keymap.set("i", "<M-c>", "<Esc>")
+
+-- Remove this background process nonesense
+vim.keymap.set({ "n", "i", "v" }, "<C-z>", "")
